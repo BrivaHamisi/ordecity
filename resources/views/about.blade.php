35ebf28f3a658e7a9e6467f749548fbac09977eb
BrivaHamisi <brivahamisi@gmail.com>
2025-06-17 16:18:05 +0300

    Organized the bout section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     @include('components.home.hero')
+    @include('components.aboutpage.approach')
     @include('components.aboutpage.ourservices')
     @include('components.aboutpage.ourteam')
-    @include('components.aboutpage.approach')
 </x-layout>
