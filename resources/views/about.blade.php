4dbe9c1fbc77bdfe222ae88ddc1982387d47bdfe
BrivaHamisi <brivahamisi@gmail.com>
2025-06-17 15:22:23 +0300

    Our Approach

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,10 @@
 <x-layout>
 
     @include('components.home.hero')
-    @include('components.about.ourapproach')
+    {{-- @include('components.about.ourapproach') --}}
     @include('components.about.whatwedo')
     @include('components.about.team')
+    @include('components.about.ourapproach')
     {{-- @include('components.home.faqs') --}}
 
 </x-layout>
