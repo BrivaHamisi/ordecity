13ae9c4dd87500045ec127253b7e5b2b66172fb3
BrivaHamisi <brivahamisi@gmail.com>
2025-06-17 15:57:18 +0300

    Added the Aboutpage directory while also reworking our services to see if that section will work

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,4 @@
 <x-layout>
     @include('components.home.hero')
-    {{-- @include('components.about.ourapproach') --}}
-    {{-- @include('components.about.whatwedo') --}}
-    {{-- @include('components.about.team') --}}
+    @include('components.aboutpage.ourservices')
 </x-layout>
