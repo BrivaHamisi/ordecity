d4ffe0aafc3cc73cdbb2f056c84d07517338fe53
BrivaHamisi <brivahamisi@gmail.com>
2025-06-17 15:36:46 +0300

    About Folder renamed

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,10 +1,6 @@
 <x-layout>
-
     @include('components.home.hero')
-    {{-- @include('components.about.ourapproach') --}}
-    {{-- @include('components.about.ourapproach') --}}
+    @include('components.about.ourapproach')
     @include('components.about.whatwedo')
     @include('components.about.team')
-    {{-- @include('components.home.faqs') --}}
-
 </x-layout>
