8ebba71a3828140d080c68d27ff6df663754959d
BrivaHamisi <brivahamisi@gmail.com>
2025-06-17 15:45:25 +0300

    About

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     @include('components.home.hero')
-    @include('components.about.ourapproach')
-    @include('components.about.whatwedo')
-    @include('components.about.team')
+    {{-- @include('components.about.ourapproach') --}}
+    {{-- @include('components.about.whatwedo') --}}
+    {{-- @include('components.about.team') --}}
 </x-layout>
