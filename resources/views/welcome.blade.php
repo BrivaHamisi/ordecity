90c251041e3f740ec0472e7a4372ca4d674bac53
BrivaHamisi <brivahamisi@gmail.com>
2025-10-19 02:16:38 +0300

    Whatsapp Floating action button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
     @include('components.home.hero')
     @include('components.home.mission&vision')
-    @include('components.home.testimonials')
+    {{-- @include('components.home.testimonials') --}}
     @include('components.home.faqs')
 
 </x-layout>
